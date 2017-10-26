b1bfd57582f5715f2f72138e07b6a1c85399aa5a
Marcin <marcinborek9@gmail.com>
2017-10-26 11:13:36 +0200

    poprawiony js/html

--- js.js
+++ js.js
@@ -1,6 +1,3 @@
-/**
- * Pobiera dane o książkach z serwera
- */
 function get_response(search_string){
 	let http = new XMLHttpRequest();
 	let request = "https://gwo.pl/booksApi/v1/search?query=" + encodeURIComponent(search_string);
@@ -10,40 +7,31 @@ function get_response(search_string){
 	return JSON.parse(http.response);
 }
 
-/**
- * Czyści całość tabeli przed ponownym wyszukaniem
- */
 function clearTable() {
 	document.getElementById('wrapper-body').innerHTML = '';
 }
 
-/**
- * Buduje tabele z danymi, ktore użytkownik chce otrzymać z serwera
- */
 function buildTable() {
 clearTable();
 	let input = document.getElementById("search-input");
 	let obj = get_response(input.value);
-
+	
 	for (let i in obj) {
-		document.getElementById('wrapper-body').innerHTML += "<tr " + "class='row-wrapper '" + ">" + 
-		"<td>" + "<img " + "class='table-image '" + "src=" + obj[i].cover + ">" + 
-		"<button " + "class='btn redirectBtn'" + ">" + "<a href='" + obj[i].url + "'" + ">" +  "Przejdź do księgarni" + "</a>"  + "</button>" + 
-		"</td><td>" + obj[i].title + 
-		"</td><td>" + obj[i].author + 
-		"</td><td>" + obj[i].isbn + 
-		"</td><td>" + obj[i].men + 
+		document.getElementById('wrapper-body').innerHTML += "<tr " + "class='row-wrapper '" + ">" +
+		"<td>" + "<img " + "class='table-image '" + "src=" + obj[i].cover + ">" +
+		"<button " + "class='btn redirectBtn'" + ">" + "<a href='" + obj[i].url + "'" + ">" +  "Przejdź do księgarni" + "</a>"  + "</button>" +
+		"</td><td>" + obj[i].title +
+		"</td><td>" + obj[i].author +
+		"</td><td>" + obj[i].isbn +
+		"</td><td>" + obj[i].men +
 		"</td><td>" + obj[i].pages_count +
-		"</td><td>" + obj[i].levels[0].school + ', ' + obj[i].levels[0].class
-		"</td><td>" + obj[i].subject + 
-		"</td><td>" + obj[i].type + 
+		"</td><td>" + obj[i].levels[0].school + ', ' + obj[i].levels[0].class +
+		"</td><td>" + obj[i].subject +
+		"</td><td>" + obj[i].type +
 		"</td></tr>" + "</tr>";
 	}
 }
 
-/**
- * Wyszukiwanie na klawisz enter
- */
 function onKeyFunction(e){
 	if(e.keyCode === 13){
 		e.preventDefault();
